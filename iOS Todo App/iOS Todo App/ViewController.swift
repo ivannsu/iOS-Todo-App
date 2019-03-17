0e715d8803f4d55f4fbaf17aa40588c47e66fbb4
ivannsu <ivansu.webmail@gmail.com>
2019-03-17 22:09:30 +0700

    Init xocde project

---
+++ iOS Todo App/iOS Todo App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Todo App
+//
+//  Created by Ivan Su on 3/17/19.
+//  Copyright © 2019 Ivan Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
