900f9a255faee825f599508e5869989fd07ee011
ivannsu <ivansu.webmail@gmail.com>
2019-03-18 08:07:04 +0700

    success show sample data in tableView

--- iOS Todo App/iOS Todo App/Controllers/TodoViewController.swift
+++ iOS Todo App/iOS Todo App/Controllers/TodoViewController.swift
@@ -11,10 +11,39 @@ import UIKit
 class TodoViewController: UIViewController {
 
     @IBOutlet weak var itemsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Perform Delegate
+        initDelegate()
+        
+        // Register NIB file for Cell
+        registerComponent()
     }
-
-
+    
+    func initDelegate() {
+        itemsTableView.delegate = self
+        itemsTableView.dataSource = self
+    }
+    
+    func registerComponent() {
+        itemsTableView.register(UINib(nibName: "TodoItemCell", bundle: nil), forCellReuseIdentifier: "toDoItemCell")
+    }
 }
 
+extension TodoViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath) as! TodoItemCell
+        
+        cell.titleLabel.text = "Hello"
+        
+        return cell
+    }
+    
+    
+}
---
+++ iOS Todo App/iOS Todo App/Models/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  iOS Todo App
+//
+//  Created by Ivan Su on 3/18/19.
+//  Copyright © 2019 Ivan Su. All rights reserved.
+//
+
+import Foundation
