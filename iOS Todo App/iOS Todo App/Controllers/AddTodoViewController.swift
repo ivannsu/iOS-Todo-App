b57e0d0710abde04a3e9b07d10e8e02200d59421
ivannsu <ivansu.webmail@gmail.com>
2019-03-18 09:13:43 +0700

    added new controller: add todo

---
+++ iOS Todo App/iOS Todo App/Controllers/AddTodoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddTodoViewController.swift
+//  iOS Todo App
+//
+//  Created by Ivan Su on 3/18/19.
+//  Copyright © 2019 Ivan Su. All rights reserved.
+//
+
+import UIKit
+
+class AddTodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
