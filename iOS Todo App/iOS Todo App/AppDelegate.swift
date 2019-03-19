8beb39f3bc0473a99cc5af62171cd20827b9cb11
ivannsu <ivansu.webmail@gmail.com>
2019-03-19 10:32:14 +0700

    refactor datamodel initialize

--- iOS Todo App/iOS Todo App/AppDelegate.swift
+++ iOS Todo App/iOS Todo App/AppDelegate.swift
@@ -53,7 +53,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Core Data stack
     
     lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "CoreData")
+        let container = NSPersistentContainer(name: "DataModel")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
